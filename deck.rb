8fedaab4acb9111a9b028888d51f9f5c3eef7f51
viktor <bvv33@yandex.ru>
2022-01-03 12:04:33 +0200

    created class "Deck"

---
+++ deck.rb
@@ -0,0 +1,12 @@
+class Deck
+  attr_reader :cards
+
+  def initialize
+    @cards = []
+    Card::SUITS.each do |suit|
+      Card::RANK.each{ |rank, value| @cards << Card.new(suit, rank, value) }
+    end
+    @cards.shuffle!
+  end
+end
+
