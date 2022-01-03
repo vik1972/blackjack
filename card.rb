89f3082115e9ca6add411b9c4a62d7665288cf06
viktor <bvv33@yandex.ru>
2022-01-03 11:01:14 +0200

    created class "Card"

---
+++ card.rb
@@ -0,0 +1,26 @@
+class Card
+  attr_accessor :suit, :rank, :value
+
+  SUITS = ["\u2660" , "\u2665", "\u2663", "\u2666"]
+  RANK = {
+    "2" => 2,
+    "3" => 3,
+    "4" => 4,
+    "5" => 5,
+    "6" => 6,
+    "7" => 7,
+    "8" => 8,
+    "9" => 9,
+    "10" => 10,
+    "В" => 10,
+    "Д" => 10,
+    "К" => 10,
+    "Т" => 11
+  }
+  
+  def initialize(suit, rank, value)
+    @suit = suit
+    @rank = rank
+    @value = value
+  end 
+end
