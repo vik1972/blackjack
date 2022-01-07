7a29aaa4d08c2408bb844a80f1811d9e50647742
viktor <bvv33@yandex.ru>
2022-01-07 14:54:14 +0200

    created file main

---
+++ main.rb
@@ -0,0 +1,10 @@
+require_relative "box"
+require_relative "card"
+require_relative "deck"
+require_relative "gamer"
+require_relative "dealer"
+require_relative "player"
+require_relative "interface"
+require_relative "game"
+
+Interface.new.start
